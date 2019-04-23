32090c97ef3e86f111d2f988cb5dc47f777211f4
voxace <dave@voxace.com>
2019-04-23 16:38:46 +1000

    Updated User API and Controller

--- api/user.js
+++ api/user.js
@@ -2,9 +2,33 @@ const Router = require("koa-router");
 const controller = require("../controllers").user;
 const router = new Router();
 
-router.post("/user", controller.post);
-router.get("/user", controller.get);
-router.put("/user", controller.put);
-router.delete("/user", controller.delete);
+/* ~~~~~~~~~~~~~~~~~~~~ POSTS ~~~~~~~~~~~~~~~~~~~~ */
+
+// Add new user
+router.post("/user", controller.addUser);
+
+/* ~~~~~~~~~~~~~~~~~~~~ GETS ~~~~~~~~~~~~~~~~~~~~ */
+
+// Get all users
+router.get("/user", controller.getAllUsers);
+
+// Get single user by ID
+router.get("/user/id/:id", controller.getUserByID);
+
+// Get single user by Name
+router.get("/user/name/:name", controller.getUserByName);
+
+// Get single user by Gamertag
+router.get("/user/gamertag/:tag", controller.getUserByGamertag);
+
+/* ~~~~~~~~~~~~~~~~~~~~ PATCHES ~~~~~~~~~~~~~~~~~~~~ */
+
+// Update user by ID
+router.patch("/user/:id", controller.patchUserByID);
+
+/* ~~~~~~~~~~~~~~~~~~~~ DELETES ~~~~~~~~~~~~~~~~~~~~ */
+
+// Delete user by ID
+router.delete("/user/:id", controller.deleteUserByID);
 
 module.exports = router.routes();
--- controllers/user.js
+++ controllers/user.js
@@ -4,28 +4,117 @@ const async = require("async");
 
 module.exports = {
 
-  async post(ctx) {
+  /* ~~~~~~~~~~~~~~~~~~~~ CREATE ~~~~~~~~~~~~~~~~~~~~ */
+
+  // Add new user
+  async addUser(ctx) {
     let name = ctx.request.body.name;
     let gamertag = ctx.request.body.gamertag;
     let newUser = new Model.user({ name: name, gamertag: gamertag});
     await newUser
       .save()
       .then(result => {
-        ctx.body = result;
+        if(result) { ctx.body = result; }
+        else { throw "Error saving user"; }
       })
+      .catch(error => {
+        throw new Error(error);
+      });
   },
-  async get(ctx) {
+
+  /* ~~~~~~~~~~~~~~~~~~~~ READ ~~~~~~~~~~~~~~~~~~~~ */
+
+  // Get all users
+  async getAllUsers(ctx) {
     await Model.user
       .find({})
       .then(result => {
-        ctx.body = result;
+        if(result) { ctx.body = result; }
+        else { throw "No users found"; }
       })
+      .catch(error => {
+        throw new Error(error);
+      });
   },
-  async put(ctx) {
-    ctx.body = "PUT";
+
+  // Get single user by ID
+  async getUserByID(ctx) {
+    let id = ctx.params.id;
+    await Model.user
+      .findOne({ _id: id })
+      .then(result => {
+        if(result) { ctx.body = result; }
+        else { throw "User not found"; }
+      })
+      .catch(error => {
+        throw new Error(error);
+      });
   },
-  async delete(ctx) {
-    ctx.body = "DELETE";
+
+  // Get single user by Name
+  async getUserByName(ctx) {
+    let name = ctx.params.name;
+    await Model.user
+      .findOne({ name: name })
+      .then(result => {
+        if(result) { ctx.body = result; }
+        else { throw "User not found"; }
+      })
+      .catch(error => {
+        throw new Error(error);
+      });
+  },
+
+  // Get single user by Gamertag
+  async getUserByGamertag(ctx) {
+    let tag = ctx.params.tag;
+    await Model.user
+      .findOne({ gamertag: tag })
+      .then(result => {
+        if(result) { ctx.body = result; }
+        else { throw "User not found"; }
+      })
+      .catch(error => {
+        throw new Error(error);
+      });
+  },
+
+  /* ~~~~~~~~~~~~~~~~~~~~ UPDATE ~~~~~~~~~~~~~~~~~~~~ */
+
+  // Update user by ID
+  async patchUserByID(ctx) {
+    let id = ctx.params.id;
+    let user = ctx.request.body.user;
+    await Model.user
+      .updateOne({ _id: id }, {
+        name: user.name,
+        gamertag: user.gamertag,
+        avatar: user.avatar
+      })
+      .then(result => {
+        if(result.nModified > 0) { ctx.body = "Update Successful"; }
+        else if(result.nModified == 0) { ctx.body = "Nothing to change"; }
+        else { throw "Error updating user"; }
+      })
+      .catch(error => {
+        throw new Error(error);
+      });
+  },
+
+  /* ~~~~~~~~~~~~~~~~~~~~ DELETE ~~~~~~~~~~~~~~~~~~~~ */
+
+  // Delete user by ID
+  async deleteUserByID(ctx) {
+    let id = ctx.params.id;
+    await Model.user
+      .deleteOne({ _id: id })
+      .then(result => {
+        if(result.deletedCount > 0) { ctx.body = "Delete Successful"; }
+        else { throw "Error deleting user"; }
+      })
+      .catch(error => {
+        throw new Error(error);
+      });
   },
   
 };
