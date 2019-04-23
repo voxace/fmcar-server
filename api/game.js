d7f9da9a43400f799a7095054c5c7ac90510af3f
voxace <dave@voxace.com>
2019-04-23 19:28:20 +1000

    Updated Game API and Controller

--- api/game.js
+++ api/game.js
@@ -2,9 +2,33 @@ const Router = require("koa-router");
 const controller = require("../controllers").game;
 const router = new Router();
 
-router.post("/game", controller.post);
-router.get("/game", controller.get);
-router.put("/game", controller.put);
-router.delete("/game", controller.delete);
+/* ~~~~~~~~~~~~~~~~~~~~ POSTS ~~~~~~~~~~~~~~~~~~~~ */
+
+// Add new game
+router.post("/game", controller.addGame);
+
+/* ~~~~~~~~~~~~~~~~~~~~ GETS ~~~~~~~~~~~~~~~~~~~~ */
+
+// Get all games
+router.get("/game", controller.getAllGames);
+
+// Get single game by ID
+router.get("/game/id/:id", controller.getGameByID);
+
+// Get single game by Name
+router.get("/game/name/:name", controller.getGameByName);
+
+/* ~~~~~~~~~~~~~~~~~~~~ PATCHES ~~~~~~~~~~~~~~~~~~~~ */
+
+// Update game name by ID
+router.patch("/game/:id", controller.patchGameNameByID);
+
+// Update game logo by ID
+router.patch("/game/:id", controller.patchGameLogoByID);
+
+/* ~~~~~~~~~~~~~~~~~~~~ DELETES ~~~~~~~~~~~~~~~~~~~~ */
+
+// Delete game by ID
+router.delete("/game/:id", controller.deleteGameByID);
 
 module.exports = router.routes();
--- controllers/game.js
+++ controllers/game.js
@@ -4,27 +4,119 @@ const async = require("async");
 
 module.exports = {
 
-  async post(ctx) {
+  /* ~~~~~~~~~~~~~~~~~~~~ CREATE ~~~~~~~~~~~~~~~~~~~~ */
+
+  // Add new game
+  async addGame(ctx) {
     let name = ctx.request.body.name;
-    let newGame = new Model.game({ name: name });
+    let gamertag = ctx.request.body.gamertag;
+    let newGame = new Model.game({ name: name, gamertag: gamertag});
     await newGame
       .save()
       .then(result => {
-        ctx.body = result;
+        if(result) { ctx.body = result; }
+        else { throw "Error saving game"; }
       })
+      .catch(error => {
+        throw new Error(error);
+      });
   },
-  async get(ctx) {
+
+  /* ~~~~~~~~~~~~~~~~~~~~ READ ~~~~~~~~~~~~~~~~~~~~ */
+
+  // Get all games
+  async getAllGames(ctx) {
     await Model.game
       .find({})
       .then(result => {
-        ctx.body = result;
+        if(result) { ctx.body = result; }
+        else { throw "No games found"; }
       })
-  },
-  async put(ctx) {
-    ctx.body = "PUT";
-  },
-  async delete(ctx) {
-    ctx.body = "DELETE";
+      .catch(error => {
+        throw new Error(error);
+      });
   },
 
+  // Get single game by ID
+  async getGameByID(ctx) {
+    let id = ctx.params.id;
+    await Model.game
+      .findOne({ _id: id })
+      .then(result => {
+        if(result) { ctx.body = result; }
+        else { throw "Game not found"; }
+      })
+      .catch(error => {
+        throw new Error(error);
+      });
+  },
+
+  // Get single game by Name
+  async getGameByName(ctx) {
+    let name = ctx.params.name;
+    await Model.game
+      .findOne({ name: name })
+      .then(result => {
+        if(result) { ctx.body = result; }
+        else { throw "Game not found"; }
+      })
+      .catch(error => {
+        throw new Error(error);
+      });
+  },
+
+  /* ~~~~~~~~~~~~~~~~~~~~ UPDATE ~~~~~~~~~~~~~~~~~~~~ */
+
+  // Update game name by ID
+  async patchGameNameByID(ctx) {
+    let id = ctx.params.id;
+    let name = ctx.request.body.name;
+    await Model.game
+      .updateOne({ _id: id }, {
+        name: name
+      })
+      .then(result => {
+        if(result.nModified > 0) { ctx.body = "Name update Successful"; }
+        else if(result.nModified == 0) { ctx.body = "Nothing to change"; }
+        else { throw "Error updating game name"; }
+      })
+      .catch(error => {
+        throw new Error(error);
+      });
+  },
+
+  // Update game logo by ID
+  async patchGameLogoByID(ctx) {
+    let id = ctx.params.id;
+    let logo = ctx.request.body.logo;
+    await Model.game
+      .updateOne({ _id: id }, {
+        logo: logo
+      })
+      .then(result => {
+        if(result.nModified > 0) { ctx.body = "Logo update Successful"; }
+        else if(result.nModified == 0) { ctx.body = "Nothing to change"; }
+        else { throw "Error updating game logo"; }
+      })
+      .catch(error => {
+        throw new Error(error);
+      });
+  },
+
+  /* ~~~~~~~~~~~~~~~~~~~~ DELETE ~~~~~~~~~~~~~~~~~~~~ */
+
+  // Delete game by ID
+  async deleteGameByID(ctx) {
+    let id = ctx.params.id;
+    await Model.game
+      .deleteOne({ _id: id })
+      .then(result => {
+        if(result.deletedCount > 0) { ctx.body = "Delete Successful"; }
+        else { throw "Error deleting game"; }
+      })
+      .catch(error => {
+        throw new Error(error);
+      });
+  },
+  
 };
