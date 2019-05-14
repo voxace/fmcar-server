149518d6bf8a32a904b605adf44179270ab31447
voxace <dave@voxace.com>
2019-05-14 19:57:48 +1000

    Update team.js

--- models/team.js
+++ models/team.js
@@ -3,12 +3,14 @@ const Schema = mongoose.Schema;
 
 // Create Scheme
 const TeamSchema = new Schema({
-  name: { type: String, unique: true, required: true },
-  driver_a: { type: Schema.Types.ObjectId, ref: 'User' },
+  name: { type: String, required: true },
+  driver_a: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
   driver_b: { type: Schema.Types.ObjectId, ref: 'User' },
   season: { type: Schema.Types.ObjectId, ref: 'Season' },
   registered: { type: Date, default: Date.now }
 });
+
+TeamSchema.index({name: 1, season: 1}, {unique: true});
 
 // Create model
 const Team = mongoose.model("Team", TeamSchema);
